4240dc571265f1c4897320ffba8a15abc224c73c
mohammedafsalk <techafs456@gmail.com>
2023-06-04 00:03:31 +0530

    skeleton on progress

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,22 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import cors from "cors";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.js";
+import adminRouter from "./routes/admin.js";
+import dbConnect from "./config/dbConnect.js";
 
+dbConnect();
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve() + "/public"));
+
 dotenv.config();
+
+app.use("/", userRouter);
+app.use("/admin", adminRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("server running on port 5000");
