5dd5bcf10bb500624430616f8a02b210ac81c4ac
mohammedafsalk <techafs456@gmail.com>
2023-06-06 11:41:19 +0530

    user login and logout completed

--- frontend/src/Components/userHome/UserHome.jsx
+++ frontend/src/Components/userHome/UserHome.jsx
@@ -3,9 +3,23 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
+import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
 
 const UserHome = () => {
+  const dispatch = useDispatch();
+  const baseImgUrl = "http://localhost:5000/uploads/";
+  const value = useSelector((state) => {
+    return state.user;
+  });
+
+  async function handleLogout() {
+    let data = await axios.get("/logout");
+    console.log(data);
+    dispatch({ type: 'refresh' });
+  }
+
   return (
     <Card
       sx={{
@@ -18,8 +32,8 @@ const UserHome = () => {
     >
       <CardContent>
         <Avatar
-          src=""
-          alt=''
+          src={baseImgUrl + value.details.profile}
+          alt=""
           sx={{
             width: 200,
             height: 200,
@@ -30,18 +44,21 @@ const UserHome = () => {
           }}
         />
         <Typography variant="h4" component="div" align="center" gutterBottom>
-          user.name
+          {value.details.name}
         </Typography>
         <Typography color="text.secondary" align="center" gutterBottom>
-          user.jobTitle
-        </Typography>
-        <Typography variant="body2" align="center" gutterBottom>
-          user.bio
+          {value.details.email}
         </Typography>
         <Button variant="contained" fullWidth sx={{ marginBottom: 1 }}>
           Change Photo
         </Button>
-        <Button variant="outlined" color="error" fullWidth sx={{ marginBottom: 1 }}>
+        <Button
+          variant="outlined"
+          color="error"
+          fullWidth
+          sx={{ marginBottom: 1 }}
+          onClick={handleLogout}
+        >
           Logout
         </Button>
       </CardContent>
--- frontend/src/Components/userLogin/UserLogin.jsx
+++ frontend/src/Components/userLogin/UserLogin.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
+import axios from "axios";
+import { useDispatch } from "react-redux";
 
 function UserLogin() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  function validate() {
+    if (
+      email.replaceAll(" ", "") === "" ||
+      password.replaceAll(" ", "") === ""
+    ) {
+      return true;
+    }
+    return false;
+  }
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!validate()) {
+      let { data } = await axios.post("/login", { email, password });
+      if (!data.error) {
+        dispatch({ type: "refresh" });
+      }
+    }
+  }
+
   return (
     <div
       style={{
@@ -18,24 +43,29 @@ function UserLogin() {
         <Typography variant="h4" align="center" gutterBottom>
           Log In
         </Typography>
-        <form>
+        <form onClick={handleSubmit}>
           <TextField
             label="Email"
             type="email"
-            // value={username}
-            // onChange={handleUsernameChange}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             fullWidth
             margin="normal"
           />
           <TextField
             label="Password"
             type="password"
-            // value={password}
-            // onChange={handlePasswordChange}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             fullWidth
             margin="normal"
           />
-          <Button type="submit" variant="contained" color="primary">
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={validate()}
+          >
             Login
           </Button>
           <Typography variant="body2" align="center" marginTop={1}>
