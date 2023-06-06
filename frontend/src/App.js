afa80069f0e10f04ccdd725847306c8f8e5343dc
mohammedafsalk <techafs456@gmail.com>
2023-06-06 10:19:16 +0530

    functionalities started

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,58 @@
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
 import "./App.css";
 import axios from "axios";
 import UserHome from "./Components/userHome/UserHome";
 import UserLogin from "./Components/userLogin/UserLogin";
 import UserRegister from "./Components/UserRegister/UserRegister";
-
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 
 function App() {
   axios.defaults.baseURL = "http://localhost:5000/";
   axios.defaults.withCredentials = true;
 
+  const { user, admin, refresh } = useSelector((state) => {
+    return state;
+  });
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    (async function () {
+      let { data } = await axios.get("/checkAuth");
+      dispatch({
+        type: "user",
+        payload: { login: data.loggedIn, details: data.user },
+      });
+    })();
+  }, [refresh]);
+
   return (
     <Router>
-      <Routes>
-      <Route path="/" element={<UserHome/>} />
-      <Route path="/login" element={<UserLogin/>} />
-      <Route path="/register" element={<UserRegister/>} />
-      </Routes>
+      {user.login === false && (
+        <Routes>
+          <Route path="/login" element={<UserLogin />} />
+          <Route path="/register" element={<UserRegister />} />
+          <Route path="/" element={<Navigate to="/login" replace={true} />} />
+        </Routes>
+      )}
+      {user.login === true && (
+        <Routes>
+          <Route path="/" element={<UserHome />} />
+          <Route path="/login" element={<Navigate to="/" replace={true} />} />
+          <Route
+            path="/register"
+            element={<Navigate to="/" replace={true} />}
+          />
+        </Routes>
+      )}
     </Router>
   );
 }
--- frontend/src/Components/UserRegister/UserRegister.jsx
+++ frontend/src/Components/UserRegister/UserRegister.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
+import axios from "axios";
 
 function UserRegister() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errMessage, setErrMessage]=useState(null)
+
+
+  const dispatch = useDispatch();
+
+  function validate() {
+    if (
+      email.replaceAll(" ", "") === "" ||
+      password.replaceAll(" ", "") === "" ||
+      name.replaceAll(" ", "") === ""
+    ) {
+      return true;
+    }
+    return false;
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    if (!validate()) {
+      let { data } = await axios.post("/register", {
+        name,
+        email,
+        password,
+      });
+      if (!data.error) {
+        dispatch({ type: "refresh" });
+      } else {
+        setErrMessage(data.message);
+      }
+    }
+  }
+
   return (
     <div
       style={{
@@ -18,31 +55,36 @@ function UserRegister() {
         <Typography variant="h4" align="center" gutterBottom>
           Register Account
         </Typography>
-        <form>
+        <form onSubmit={handleSubmit}>
           <TextField
             label="Username"
-            // value={username}
-            // onChange={handleUsernameChange}
+            value={name}
+            onChange={(e) => setName(e.target.value)}
             fullWidth
             margin="normal"
           />
           <TextField
             label="Email"
             type="email"
-            // value={username}
-            // onChange={handleUsernameChange}
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
             fullWidth
             margin="normal"
           />
           <TextField
             label="Password"
             type="password"
-            // value={password}
-            // onChange={handlePasswordChange}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             fullWidth
             margin="normal"
           />
-          <Button type="submit" variant="contained" color="primary">
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            disabled={validate()}
+          >
             Save
           </Button>
           <Typography variant="body2" align="center" marginTop={1}>
